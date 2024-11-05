f0531afe63fc9cc7e333c72a9a2173d547a50de4
FearGod12 <onyenikechukwudi@.gmail.com>
2024-11-05 21:38:17 +0100

    updated database connection

--- backend/dist/src/swagger/swaggerConfig.js
+++ backend/dist/src/swagger/swaggerConfig.js
@@ -6,36 +6,12 @@ const options = {
             title: 'STUDY COMPANION API',
             version: '1.0.0',
             description: 'Documentation of API endpoints for Study Companion application.',
-            contact: {
-                name: 'API Support',
-                url: 'https://www.studycompanion.com/support',
-                email: 'support@studycompanion.com',
-            },
-            license: {
-                name: 'Apache 2.0',
-                url: 'https://www.apache.org/licenses/LICENSE-2.0.html',
-            },
         },
-        // servers: [
-        //   {
-        //     url: 'https://api.studycompanion.com/v1',
-        //     description: 'Production server',
-        //   },
-        //   {
-        //     url: 'https://staging-api.studycompanion.com/v1',
-        //     description: 'Staging server',
-        //   },
-        //   {
-        //     url: 'http://localhost:3000/v1',
-        //     description: 'Local development server',
-        //   },
-        // ],
         tags: [
             {
                 name: 'Users',
                 description: 'User management operations',
             },
-            // Add more tags as needed for other API categories
         ],
         components: {
             securitySchemes: {
