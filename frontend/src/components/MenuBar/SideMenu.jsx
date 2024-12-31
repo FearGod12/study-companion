04d07a7f3bf28621eaf3b03b7152fc39bdc63a8f
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-12-31 14:20:02 +0100

    updated styling

--- frontend/src/components/MenuBar/SideMenu.jsx
+++ frontend/src/components/MenuBar/SideMenu.jsx
@@ -3,6 +3,7 @@ import { RiDashboardLine } from "react-icons/ri";
 import LightLogo from "../common/LightLogo";
 import Pro from "./Pro";
 import MenuItem from "./MenuItem";
+import { Link } from "react-router-dom";
 
 const SideMenu = () => {
   return (
@@ -20,12 +21,14 @@ const SideMenu = () => {
             <h2 className="font-semibold wrap text-sm font-mono lg:block md:block hidden">
               Create New Session
             </h2>
-            <div className="lg:px-3 lg:py-1 md:px-3 md:py-1 rounded-full flex justify-center items-center lg:bg-secondary md:bg-secondary bg-gray-200 lg:text-gray-100 md:text-gray-100 text-secondary p-4">
+            <Link to='/schedule'>
+            <div className="lg:p-3 md:p-3 rounded-full flex justify-center items-center lg:bg-secondary md:bg-secondary bg-gray-200 lg:text-gray-100 md:text-gray-100 text-secondary">
               <FaPlus size={14} />
-              <span className="absolute left-1/2 transform -translate-x-1/4 bottom-full mb-2 w-max px-2 py-1 text-sm text-gray-100 bg-gray-900 rounded opacity-0 group-hover:opacity-100">
-                Create Session
-              </span>
             </div>
+            </Link>
+            <span className="absolute left-1/2 transform -translate-x-1/4 bottom-full mb-2 w-max px-2 py-1 text-sm text-gray-100 bg-gray-900 rounded opacity-0 group-hover:opacity-100">
+                Create Session
+            </span>
           </div>
           <ul className="flex flex-col gap-2 ml-4">
             <MenuItem
