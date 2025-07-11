ce9157658255e1fe1762992e90ccb4a8b7a15fa6
FearGod12 <onyenikechukwudi@gmail.com>
2025-07-11 13:41:27 +0100

    updated the names of the frontend folders

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const prisma = new PrismaClient();
 async function startServer() {
   try {
 
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 3001;
     const HOST = process.env.HOST || '0.0.0.0';
     const server = createServer(app);
 
